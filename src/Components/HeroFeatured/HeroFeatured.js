5805bb5b09ea3ceb59647353a6ac219459cd2607
Marcin Witowski <mwitowski@redlock.pl>
2024-03-07 23:07:16 +0100

    update

---
+++ src/Components/HeroFeatured/HeroFeatured.js
@@ -0,0 +1,58 @@
+import React, { useState, useEffect } from 'react';
+import { getBasicHeroInfoById } from '../../requests';
+import './HeroFeatured.css';
+
+const featuredHeroesIds = [10, 502, 505]
+
+function HeroFeatured() {
+    useEffect(() => {
+        fetchAndRenderFeauturedHeroes();
+        
+      }, []);
+    
+    const [featuredHeroesList, setFeaturedHeroesList] = useState();
+    
+    const fetchAndRenderFeauturedHeroes = async () => {
+    let heroes = [];
+
+    for (const heroId of featuredHeroesIds){
+        // getBasicHeroInfoById(heroId).then(hero => {
+        //   heroes.push(hero);
+        // })
+        const data = await getBasicHeroInfoById(heroId);
+        heroes.push(data);
+    }
+    console.log(heroes);
+    setFeaturedHeroesList(heroes);
+    }
+
+    return (
+        <section className='featured'>
+            {featuredHeroesList?.map(({ name, imgUrl, powerstats }) => (
+                <div className='featured__hero'>
+                    <h2>{name}</h2>
+                    <img src={imgUrl} alt={`${name}`} />
+                    <div className='featured__hero__stats'>
+                        <div>
+                            <p>{powerstats.combat}</p>
+                        </div>
+                        <div>
+                            <p>{powerstats.durability}</p>
+                        </div>
+                        <div>
+                            <p>{powerstats.intelligence}</p>
+                        </div>
+                        <div>
+                            <p>{powerstats.speed}</p>
+                        </div>
+                        <div>
+                            <p>{powerstats.strength}</p>
+                        </div>
+                    </div>
+                </div>
+            ))}
+        </section>
+    );
+}
+
+export default HeroFeatured;
