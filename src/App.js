d2d1aacc1c2e8f719356cf4ca21e8c166eb9cca0
Marcin Witowski <mwitowski@redlock.pl>
2024-03-09 17:16:23 +0100

    router

--- src/App.js
+++ src/App.js
@@ -1,15 +1,27 @@
 import './App.css';
 import Nav from './Components/Nav/Nav'
 import HeroesFeatured from './Components/HeroesFeatured/HeroesFeatured';
+import {
+  BrowserRouter as Router,
+  Routes,
+  Route,
+} from 'react-router-dom';
 
 function App() {
   return (
     <>
-    <Nav></Nav>
-    <main>
-      <HeroesFeatured />
-    </main>
-    <footer></footer>
+    <Router>
+      <Nav></Nav>
+      <main>
+        <div className="container">
+          <Routes>
+            <Route exact path="/" element={<HeroesFeatured />} />
+            <Route path="/hero/:id" />
+          </Routes>
+        </div>
+      </main>
+      <footer></footer>
+    </Router>
     </>
   );
 }
--- src/Components/Nav/Nav.js
+++ src/Components/Nav/Nav.js
@@ -1,10 +1,23 @@
-import React from 'react';
+import React, { useState } from 'react';
+import logo from '../../assets/img/nav_logo.png';
 import './Nav.css';
+import {
+    Link
+} from 'react-router-dom';
 
 function Nav() {
+    const [searchInputValue, setSearchInputValue] = useState('');
 
     return (
-        <nav></nav>
+        <nav className="nav">
+            <div className="container">
+                <Link to="/"><img className="nav__logo" src={logo} alt="Superhero Database" /></Link>
+                <div className="nav__search">
+                    <input onChange={(event) => {setSearchInputValue(event.target.value)}} value={searchInputValue} type="text" name="search" />
+                    <Link to={`/search/${searchInputValue}`}><button>find hero!</button></Link>
+                </div>
+            </div>
+        </nav>
     );
 }
 
