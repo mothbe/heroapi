cacd6aa988649e0c8df935dec387b77736f9d22e
Marcin Witowski <mwitowski@redlock.pl>
2024-03-10 22:14:17 +0100

    change to class

--- src/Components/HeroesFeatured/HeroesFeatured.js
+++ src/Components/HeroesFeatured/HeroesFeatured.js
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { getBasicHeroInfoById } from '../../requests';
 import './HeroesFeatured.css';
 import HeroSimplified from '../HeroSimplified/HeroSimplified';
@@ -13,51 +13,51 @@ function getRandomInt(max) {
 
 const featuredHeroesIds = getRandomInt(maxHeroID);
 
-function HeroesFeatured() {
+class HeroesFeatured extends React.Component {
+    constructor() {
+        super();
 
-    const [ isLoading, setLoadingState] = useState(true);
-
-    useEffect(() => {
-        setLoadingState(true);
-        fetchAndRenderFeauturedHeroes();
-        
-      }, []);
-    
-    const [featuredHeroesList, setFeaturedHeroesList] = useState();
-    
-    const fetchAndRenderFeauturedHeroes = async () => {
-    let heroes = [];
-    for (const heroId of featuredHeroesIds){
-        // getBasicHeroInfoById(heroId).then(hero => {
-        //   heroes.push(hero);
-        // })
-        const data = await getBasicHeroInfoById(heroId);
-        heroes.push(data);
-    }
-    // console.log(heroes);
-    setFeaturedHeroesList(heroes);
-    setLoadingState(false);
+        this.state = {
+            featuredHeroesList: [],
+            isLoading: true
+        }
     }
 
-    return (
-        <section className='featured'>
-            <h1>Featured Heroes</h1>
-            {
-            !isLoading && (
-                <div className="featured__list">
-                    {featuredHeroesList?.map(({ name, imgUrl, powerstats, id }) => (
-                        <HeroSimplified key={id} name={name} imgUrl={imgUrl} powerstats={powerstats} id={id} />
-                    ))}
-                </div>
-            )
-            }
-            {
-                isLoading && <div className='loader-container'>
-                        <Loader />
+    componentDidMount() {
+        this.fetchAndRenderFeauturedHeroes();
+    }
+    
+    fetchAndRenderFeauturedHeroes = async () => {
+        let heroes = [];
+        for (const heroId of featuredHeroesIds){
+            const data = await getBasicHeroInfoById(heroId);
+            heroes.push(data);
+        }
+        this.setState({featuredHeroesList: heroes, isLoading: false})
+    }
+
+    render() {
+        const { isLoading, featuredHeroesList} = this.state;
+        return (
+            <section className='featured'>
+                <h1>Featured Heroes</h1>
+                {
+                !isLoading && (
+                    <div className="featured__list">
+                        {featuredHeroesList?.map(({ name, imgUrl, powerstats, id }) => (
+                            <HeroSimplified key={id} name={name} imgUrl={imgUrl} powerstats={powerstats} id={id} />
+                        ))}
                     </div>
-            }
-        </section>
-    );
+                )
+                }
+                {
+                    isLoading && <div className='loader-container'>
+                            <Loader />
+                        </div>
+                }
+            </section>
+        );
+    }
 }
 
 export default HeroesFeatured;
